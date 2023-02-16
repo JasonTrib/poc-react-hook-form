c49874fcd8030308af789d6189d98f1d9141ad26
Iasonas Trimpalis <i.trimpalis@xe.gr>
2023-02-16 16:21:46 +0200

    install form packages, do preparation

---
+++ src/Form.tsx
@@ -0,0 +1,15 @@
+import './Form.css';
+
+function Form() {
+  return (
+    <>
+      <h1>React hook form</h1>
+      <div className="form">
+        <input type="text" placeholder="name"/>
+        <input type="password" placeholder="password"/>
+      </div>
+    </>
+  )
+}
+
+export default Form
